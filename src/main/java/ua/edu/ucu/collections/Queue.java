f26bfe1d9cd17742ad1e6a3d0eedbc25ed26ddb9
PankivAndrew <pankiv_a@ucu.edu.ua>
2016-11-08 14:19:35 +0200

    Update Queue.java

--- src/main/java/ua/edu/ucu/collections/Queue.java
+++ src/main/java/ua/edu/ucu/collections/Queue.java
@@ -1,6 +1,25 @@
 package main.java.ua.edu.ucu.collections;
 
-public class Queue {
-    public Object[] list_of_elements;
-    
+import main.java.ua.edu.ucu.collections.immutable.ImmutableLinkedList;
+import main.java.ua.edu.ucu.collections.immutable.LinkedNode;
+
+public class Queue extends ImmutableLinkedList {
+    private ImmutableLinkedList Queue;
+
+
+    public Queue(LinkedNode head) {
+        super(head);
+    }
+    public Object peek(){
+        return Queue.get(0);
+    }
+    public Object dequeue(){
+        Object new_queue = Queue.get(0);
+        Queue = Queue.remove(0);
+        return new_queue;
+    }
+    public void enqueue(Object e){
+        Queue = Queue.add(e);
+    }
+
 }
